4ed8b53512a3a5811892add70231c9941a0244df
Marius <marius.kriegerowski@zmaw.de>
2013-11-01 10:18:31 +0100

    path adjusted

--- make-random-test-traces.py
+++ make-random-test-traces.py
@@ -7,5 +7,5 @@ data1 = num.random.random(nsamples)
 data2 = num.random.random(nsamples)
 t1 = trace.Trace(network='ref', station='TEST', channel='Z', deltat=0.5, tmin=tmin, ydata=data1)
 t2 = trace.Trace(network='ref', station='TEST', channel='N', deltat=0.5, tmin=tmin, ydata=data2)
-io.save([t1,t2], 'referenceRandomTestFiles/my_precious_reference_traces.mseed')            # all traces in one file
+io.save([t1,t2], 'mseeds/referenceRandomTestFiles/my_precious_reference_traces.mseed')            # all traces in one file
 
