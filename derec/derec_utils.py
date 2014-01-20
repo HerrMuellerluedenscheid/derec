a2ab20f20c07a4ad1022bafe67b494470a0a4d2c
marius <marius.kriegerowski@zmaw.de>
2014-01-20 12:01:58 +0100

    cleanup

--- derec/derec_utils.py
+++ derec/derec_utils.py
@@ -54,112 +54,6 @@ def azi_to_location_digits(azi):
     """
     return str(int(azi)).zfill(3)
 
-
-def find_matching_traces(reference_pile, test_list):
-    """
-    Find matching pairs of traces for later Verwurstung.
-    :param reference_pile:
-    :param test_list:
-    :return: list of sets, each containing two matching traces
-    :rtype : list of sets of traces [(tr1, tr1_test) ,(tr2, tr2_test), (...),...]
-    """
-    trace_list = []
-    num_matched = 0
-    num_unmatched = 0
-    for traces_group in reference_pile:
-        for ref_trace in traces_group:
-            for test_trace in test_list:
-                if util.match_nslc('%s.%s.[0-9]-%s.%s' % (ref_trace.network,
-                                                          ref_trace.station,
-                                                          ref_trace.location,
-                                                          ref_trace.channel),
-                                   test_trace.nslc_id):
-                    logger.info('Found matching traces: %s \n %s' % (ref_trace, test_trace))
-                    trace_list.append([ref_trace, test_trace])
-                    num_matched += 1
-                    break
-                else:
-                    continue
-            else:
-                num_unmatched += 1
-                logger.warning('No matching trace found for reference trace: %s' % ref_trace)
-        if num_matched is 0:
-            raise NoMatchingTraces()
-        if num_unmatched is 0:
-            logger.info('All traces found matching trace')
-        else:
-            logger.warning('%s of %s traces unmatched' % (num_unmatched, num_matched))
-
-    return trace_list
-
-
-def chop_longer_samples(data_set):
-    """
-    chop both samples to the same length.
-    """
-    t1 = data_set[0]
-    t2 = data_set[1]
-    if np.shape(t1) > np.shape(t2):
-        t1 = np.resize(t1, np.shape(t2))
-
-    elif np.shape(t1) < np.shape(t2):
-        t2 = np.resize(t2, np.shape(t1))
-
-    return [t1, t2]
-
-def misfit_by_samples(data_set, square=False):
-    """ 
-    Calculate misfit from a set of two 1 dimensional arrays 
-
-    :rtype : np.float ??? 
-    :type square: bool 
-    :param square: in True take square root of 
-    :type data_set: set 
-    :param data_set: 
-    """ 
-    if square: 
-        exp = 2 
-    else: 
-        exp = 1
-
-    data_set = chop_longer_samples(data_set)
-
-    return np.sum(abs(pow(data_set[0], exp)-pow(data_set[1], exp)))
-
-
-def frequency_domain_misfit(reference_pile, test_list, square=False, **kwargs):
-    """ 
-
-    :param reference_pile: 
-    :param test_list:
-    :return: 
-    """ 
-    traces_sets = find_matching_traces(reference_pile, test_list)
-    spectra_sets = []
-    for tr1, tr2 in traces_sets:
-        # ignore fx-data
-        spectra_sets.append(np.array((tr1.spectrum()[1],
-                                      tr2.spectrum()[1])))
-
-    return sum(map(lambda x: misfit_by_samples(x, square=square), spectra_sets))
-
-
-def time_domain_misfit(reference_pile, test_list, square=False):
-    """
-    :param test_list:
-    :param square:
-    :type reference_pile: pile.Pile
-    :param reference_pile:
-    """
-    traces_sets = find_matching_traces(reference_pile, test_list)
-    downsample_if_needed(traces_sets)
-
-    data_sets = []
-    for traces_set in traces_sets:
-        data_sets.append(np.array((traces_set[0].ydata, traces_set[1].ydata)))
-    return sum(map(lambda x: misfit_by_samples(x, square=square), data_sets))
-
-
 def chop_ranges(model, targets, phase_start, test_sources, phase_end=None, static_offset=None):
     '''
     Create extended phase markers as preparation for chopping.
@@ -260,28 +154,6 @@ def sampling_rate_similar(t1, t2):
     return abs(t1.deltat - t2.deltat) <= t1.deltat / 100
 
 
-def downsample_if_needed(trace_pairs):
-    '''
-    Downsample high frequency trace of traces pairs to the lower of both sampling rates.
-    :param trace_pairs:
-    :return:
-
-    !!! resample ist problematisch, wenn die Frequenzen zu weit auseinanderliegen.
-    '''
-
-    ts = filter(lambda x: not sampling_rate_similar(x[0], x[1]), trace_pairs)
-    for trace_pair in ts:
-        trace_pair.sort(key=lambda x: x.deltat)
-        trace_pair[0].resample(trace_pair[1].deltat)
-
-
-def requests_in_gfdb_range(request, gfdb):
-    '''
-    Verify, that no depth in requested depths is out of range covered by the gfdb.
-    '''
-    if min(request) >= gfdb.firstz and max(request) <= gfdb.firstz + gfdb.nz * gfdb.dz:
-        return True
-
 def plot_misfit_dict(mfdict):
     plt.figure()
     print mfdict.values()
@@ -290,5 +162,3 @@ def plot_misfit_dict(mfdict):
     plt.ylabel('Misfit []')
     plt.show()
 
-def request_earthmodel(engine):
-    return
