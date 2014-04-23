daaa1f5bce38f66223d2bb9552ca1d2addf6c589
Marius <marius.kriegerowski@zmaw.de>
2014-04-23 10:44:04 +0200

    fixed bug in restitue

--- mseeds/iris_data/restitute.py
+++ mseeds/iris_data/restitute.py
@@ -1,5 +1,4 @@
 from pyrocko import trace, io
-
 '''
 restitude seismic traces using resp files. 
 
@@ -13,14 +12,25 @@ channels = ['BHE', 'BHN', 'BHZ']
 respdir = 'GSN_resp/'
 restdir = 'restitute/'
 stations_needed = 13
-i=1
+i=0
 
 done = [[]]
-for t in traces:
-    if not all(map(lambda x: done.count(x)==stations_needed, channels)):    
-        if t.channel in channels:
-            if done.count(t.nslc_id[3])== stations_needed:
-                continue
+c_left = set(channels)
+t_stack = []
+for t_i, t in enumerate(traces):
+    if not t.channel in channels:
+        t_stack = []
+        continue
+    t_stack.append(t)
+    if t.nslc_id[:3]!=t_stack[0].nslc_id[:3]:
+        t_stack = [t]
+        continue
+    if len(t_stack)==3:
+        #    if not all(map(lambda x: done.count(x)==stations_needed, channels)):    
+        #            if done.count(t.nslc_id[3])== stations_needed:
+        #                continue
+        print '-----'
+        for t in t_stack:
             tcodes = t.nslc_id
             respfile = respdir + 'RESP.%s.%s.%s.%s.'%tcodes 
             invevalresp = trace.InverseEvalresp(respdir, t)
@@ -31,5 +41,6 @@ for t in traces:
 
             io.save(tnew, restdir+'%s.%s.%s.%s'%(t.nslc_id))
             done.append(tnew.nslc_id[3])
-            i+=1
-            
+        i+=1
+        if i==stations_needed:
+            break
