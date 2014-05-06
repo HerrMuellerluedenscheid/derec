8b1ef5cb4d4de097448d6d889a5aa3a7e18c7ae4
Marius <marius.kriegerowski@zmaw.de>
2014-05-06 17:07:04 +0200

    improved restitution of noise

--- mseeds/iris_data/restitute.py
+++ mseeds/iris_data/restitute.py
@@ -1,42 +1,114 @@
 from pyrocko import trace, io
+import matplotlib.pyplot as plt
+import numpy as num
+import os
+pjoin = os.path.join
 '''
 restitude seismic traces using resp files. 
 
 Give the number of stations, that you would like to restitude as
 *stations_needed*.
 '''
+def save_noise(directory, traces):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    for t in traces:
+        io.save(traces, pjoin(directory,'%s.%s.%s.%s'%(t.nslc_id)))
+
+
+def equalize_number_of_samples(traces):
+    minlen = min([len(t.get_ydata()) for t in traces])
+    map(lambda t: t.set_ydata(t.get_ydata()[:minlen]), traces)
+        
+
+def check_noise(traces):
+    verticals = filter(lambda x: x.nslc_id[3]=='BHZ', traces)
+    horizontals = list(set(traces)-set(verticals))
+
+    checked = check_ampspecs(verticals)
+    checked.extend(check_ampspecs(horizontals))
+
+    plt.show()
+    return checked
+
+def check_ampspecs(traces, **kwargs):
+    ampspecs = []
+    checked = []
+    log_ydata = {}
+    for t in traces:
+        fx,fa = t.spectrum(tfade=20)
+        abs_fa = num.abs(fa)
+        log_abs_fa = num.log(abs_fa)
+        log_ydata.update({t:log_abs_fa})
+    log_ydata_array = num.array(log_ydata.values())
+    log_median = num.median(log_ydata_array, axis=0)
+    sigma = num.std(log_median)
+    mean_log_median = num.mean(log_median)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    for t, logdata in log_ydata.items():
+        mean_log_y = num.mean(logdata)
+        if num.abs(mean_log_median-mean_log_y)<sigma:
+            alpha = 1.
+            symbol = 'x'
+            checked.append(t)
+        else:
+            alpha = 0.05
+            symbol = '.'
+        
+        ax.plot(fx, num.exp(logdata), symbol, alpha=alpha)
+                
+    ax.plot(fx, num.exp(log_median),'--')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    plt.draw()
+    return checked
+ 
+
 
 traces = io.load('GSN.mseed')
 
 channels = ['BHE', 'BHN', 'BHZ']
 respdir = 'GSN_resp/'
 restdir = 'restitute/'
-stations_needed = 13
+stations_needed = 40
 i=0
 
-done = [[]]
+done = []
 c_left = set(channels)
-t_stack = []
+#t_stack = []
+t_stack = {}
+ready = []
 for t_i, t in enumerate(traces):
     if not t.channel in channels:
-        t_stack = []
         continue
-    t_stack.append(t)
-    if t.nslc_id[:3]!=t_stack[0].nslc_id[:3]:
-        t_stack = [t]
-        continue
-    if len(t_stack)==3:
-        print '-----'
-        for t in t_stack:
-            tcodes = t.nslc_id
-            invevalresp = trace.InverseEvalresp(respdir, t)
-            print 'restitute: %s'%t
-            tnew = t.transfer(tfade=10., 
-                              freqlimits=[0.01, 0.02, 30, 60], 
-                              transfer_function=invevalresp)
+    else:
+        try:
+            t_stack[t.nslc_id[:3]].append(t)
+            if len(t_stack[t.nslc_id[:3]])==3:
+                print '-----'
+                for t in t_stack[t.nslc_id[:3]]:
+                    t.downsample_to(0.1)
+                    tcodes = t.nslc_id
+                    invevalresp = trace.InverseEvalresp(respdir, t)
+                    print 'restitute: %s'%t
+                    tnew = t.transfer(tfade=20., 
+                                      freqlimits=[0.01, 0.02, 10, 20], 
+                                      transfer_function=invevalresp)
 
-            io.save(tnew, restdir+'%s.%s.%s.%s'%(t.nslc_id))
-            done.append(tnew.nslc_id[3])
-        i+=1
-        if i==stations_needed:
-            break
+                    done.append(t)
+                i+=1
+                if i==stations_needed:
+                    break
+        except KeyError:
+            t_stack.update({t.nslc_id[:3]: [t]})
+
+equalize_number_of_samples(done)
+checked = check_noise(done)
+
+save_noise('checked_noise', checked)
+
+
+
