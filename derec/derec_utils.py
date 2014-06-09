195682396e91dda420ef8296961f5637ef6542c5
marius <marius.kriegerowski@zmaw.de>
2014-06-09 19:23:01 +0200

    it's getting worse...

--- derec/derec_utils.py
+++ derec/derec_utils.py
@@ -12,6 +12,7 @@ from pyrocko.gf.seismosizer import *
 from pyrocko.gui_util import PhaseMarker
 from pyrocko.parimap import parimap
 from scipy import interpolate
+from derec import phase_cache as pc
 
 from multiprocessing import Pool, Pipe, Process, Manager
 from itertools import izip
@@ -141,85 +142,6 @@ def azi_to_location_digits(azi):
     return r
 
 
-def cake_first_arrival(distance, depth, model, phase_ids=None):
-    """
-    Get very first arrival of *phases*. 
-    """
-    if not phase_ids:
-        phase_ids= ['p','P', 'pP']
-    
-    phases = [cake.PhaseDef(ph) for ph in phase_ids]
-    arrivals = model.arrivals([distance*cake.m2d], 
-                            phases,
-                            zstart=depth,
-                            zstop=depth)
-
-    tmin = min(arrivals, key=lambda x: x.t).t
-    return tmin
-
-class PhaseCache():
-    def __init__(self, tmin_phase_cache=defaultdict(), 
-            tmax_phase_cache=defaultdict(), store=None, phase_ids_start=[], 
-            phase_ids_end=[]):
-
-        self.tmin_phase_cache = tmin_phase_cache
-        self.tmax_phase_cache = tmax_phase_cache
-        self.model = store.config.earthmodel_1d
-        self.store = store
-        self.phase_ids_start = phase_ids_start
-        self.phase_ids_end = phase_ids_end
-
-    def flush(self):
-        self.tmin_phase_cache = defaultdict()
-        self.tmax_phase_cache = defaultdict()
-        print 'flushedd  ' , self.tmin_phase_cache
-
-    def get_cached_tmin(self, target, source):
-        dist = source.distance_to(target)
-        key = (source.depth, dist)
-        return self.tmin_phase_cache[key]
-      
-    def get_cached_arrivals(self, target, source, static_length=0., 
-            perc=None, use_cake=True):
-        print static_length, 'gca'
-
-        dist = source.distance_to(target)
-        key = (source.depth, dist)
-        
-        try:
-            tmin = self.get_cached_tmin(target, source)
-            print 'got caches tmin', tmin
-        except KeyError:
-            if use_cake:
-                tmin = cake_first_arrival(dist, source.depth, self.model,
-                        phase_ids=self.phase_ids_start)
-                print 'didn et tmin ',  tmin
-            else:
-                tmin = self.store.t('first(%s)'% self.phase_ids_start, key)
-            self.tmin_phase_cache[key] = tmin
-
-        if self.tmax_phase_cache.get(key, False):
-            tmax = self.tmax_phase_cache[key]
-        else:
-            if perc:
-                tmax = tmin + static_length + tmin * perc / 100.
-
-            elif use_cake:
-                print 'use cake'
-                tmax = cake_first_arrival(dist, source.depth, self.model,
-                        phases=self.phase_ids_end.split('|'))
-
-            else:
-                print 'use fomosto'
-                tmax = store.t('first(%s)'%self.phase_ids_end, key)
-            self.tmax_phase_cache[key] = tmax
-
-
-        tmin += source.time
-        tmax += source.time
-
-        return tmin, tmax
-
 
 def chop_ranges(sources, targets, store, phase_ids_start,  phase_ids_end=None,
              picked_phases={}, t_shift_frac=0., cache=True, return_cache=False, **kwargs):
@@ -238,11 +160,11 @@ def chop_ranges(sources, targets, store, phase_ids_start,  phase_ids_end=None,
         perc = kwargs['perc']
     assert not phase_ids_end==perc and None in (phase_ids_end, perc)
 
-    phase_cache = PhaseCache(tmin_phase_cache=picked_phases,
+    phase_cache = pc.PhaseCache(tmin_phase_cache=picked_phases,
                              store=store, 
                              phase_ids_start=phase_ids_start,
                              phase_ids_end=phase_ids_end)
-    print 'phasecache instance, ', phase_cache
+    print 'created new phasecache instance, ', phase_cache
 
     parallelize = False 
     if kwargs.get('parallelize', False):
@@ -285,7 +207,9 @@ def chop_ranges(sources, targets, store, phase_ids_start,  phase_ids_end=None,
         phase_marker_dict[source][target] = m
 
     if not cache:
-        phase_cache.flush()
+        del phase_cache
+        print 'deleted phase_cache instance'
+        #phase_cache.flush()
 
     if return_cache:
         return phase_marker_dict, phase_cache
