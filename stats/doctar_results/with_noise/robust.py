2733f1a5342ce8a72eabfadb76d63096f9fdf422
Marius <marius.kriegerowski@zmaw.de>
2014-06-23 11:10:44 +0200

    time win with more distance dependent weighting

--- stats/doctar_results/with_noise/robust.py
+++ stats/doctar_results/with_noise/robust.py
@@ -59,14 +59,14 @@ if __name__ ==  "__main__":
     pb = None
     add_noise = True 
     verbose = True
-    debug = True
+    debug = False
     write_depth = True
 
     if test_type=='doctar':
         stf = [[0.,0.2], [0.,1.]]
         #store_id = 'crust2_m5_10Hz'
         # Das ist der neu gebaute store:
-        store_id = 'doctar_mainland_20Hz'
+        store_id = 'doctar_mainland_20Hz_200m'
         data_base_dir = pjoin(derec_home, 'mseeds', 'doctar', 'doctar_2011-11-01')
         stations_file = 'stations.txt'
         event_file = 'doctar_2011-11-01_quakefile.dat'
@@ -95,14 +95,14 @@ if __name__ ==  "__main__":
     #depths = num.linspace(_ref_source.depth-dz, _ref_source.depth+dz, num_depths)
     offset = 3000
 
-    depths = range(_ref_source.depth-offset, _ref_source.depth+offset, 1000)
+    depths = range(_ref_source.depth-offset, _ref_source.depth+offset, 200)
     print depths
     #depths=[_ref_source.depth]
 
     ref_source_moment_tensor = _ref_source.pyrocko_moment_tensor()
     location_test_sources_lists = du.make_lots_of_test_events(_ref_source, depths, 
-            {'strike':10., 'dip':10., 'rake':10., 'north_shift':3000,
-                'east_shift': 3000.}, 
+            {'strike':5., 'dip':5., 'rake':5., 'north_shift':1000,
+                'east_shift': 1000.}, 
             num_tests,
             func='normal') 
     i=0
@@ -112,7 +112,7 @@ if __name__ ==  "__main__":
     #taper = trace.CosFader(xfrac=0.333) 
     taper = trace.CosFader(xfade=1.0) 
     
-    z, p, k = butter(2, [0.7*num.pi*2, 6.5*num.pi*2.], 
+    z, p, k = butter(2, [0.7*num.pi*2, 6.0*num.pi*2.], 
                        'band', 
                        analog=True, 
                        output='zpk')
@@ -137,11 +137,11 @@ if __name__ ==  "__main__":
                                    store_id=store_id,
                                    misfit_setup=misfit_setup,
                                    source_time_function=stf,
-                                   number_of_time_shifts=61,
-                                   percentage_of_shift=45.,
+                                   number_of_time_shifts=41,
+                                   percentage_of_shift=20.,
                                    phase_ids_start=phase_ids_start,
-                                   static_length=2.8,
-                                   marker_perc_length=5.0,
+                                   static_length=1.8,
+                                   marker_perc_length=50.0,
                                    marker_shift_frac=0.50,
                                    depths=depths) 
 
@@ -169,11 +169,6 @@ if __name__ ==  "__main__":
                                                     targets, engine,
                                                     stf,
                                                     noise=noise)
-
-    #map(lambda x: x.highpass(4, 0.5), reference_seismograms.values()[0].values())
-    #map(lambda x: x.lowpass(4,10.)), reference_seismograms.values()[0].values())
-
-    #trace.snuffle(reference_seismograms.values()[0].values()[0])
 
     results = []
 
